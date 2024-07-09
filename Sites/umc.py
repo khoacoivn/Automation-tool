4c0c2fafbee1152d33797f237eac10f641c7bbf6
Thong Nguyen Huy (VN) <Thong.NguyenH1@homecredit.vn>
2024-07-09 13:34:09 +0700

    adding docstring & revamp the requirements

--- Sites/umc.py
+++ Sites/umc.py
@@ -1,5 +1,5 @@
-from Common.page_object import page_object as Page
 import logging
+from Common.page_object import page_object as Page
 
 
 # Element Path
@@ -50,27 +50,60 @@ class umc(Page):
 
     # Function
     def get_umc_url(self) -> None:
+        """
+        This method navigates to the UMC URL.
+        """
         self.get(self.umc_url)
 
+
     def login_with_data(self, ldap_user: str, ldap_pw: str) -> bool:
+        """
+        This method logs in with the provided LDAP user and password.
+
+        Args:
+            ldap_user (str): The LDAP username.
+            ldap_pw (str): The LDAP password.
+
+        Returns:
+            bool: True if login is successful, False otherwise.
+        """
         if (ldap_user is not None) & (ldap_pw is not None):
             self.search_by_xpath(self.ldap_user_input, delay=0.5).send_keys(ldap_user)
             self.search_by_xpath(self.ldap_pw_input, delay=0.5).send_keys(ldap_pw)
             return self.search_by_xpath(self.login_button, delay=0.5).click()
         else:
-            logging.critical("Cannot ")
+            logging.critical("Missing Username or Password.")
+            return False
 
     def logout(self) -> None:
+        """
+        This method logs out of the current session.
+        """
         self.search_by_xpath(self.logout_button).click()
 
     def search_hrid(self, hrid: str) -> None:
+        """
+        This method searches for a given HRID.
+
+        Args:
+            hrid (str): The HRID to search for.
+        """
         self.search_by_xpath(self.hrid_input).send_keys(hrid)
         self.search_by_xpath(self.hrid_search_button).click()
 
     def click_details_button(self) -> None:
+        """
+        This method clicks the details button.
+        """
         self.search_by_xpath(self.detail_button).click()
 
     def click_block_button(self) -> bool:
+        """
+        This method clicks the block button.
+
+        Returns:
+            bool: False if the button is flagged, True otherwise.
+        """
         button = self.search_by_xpath(self.block_button)
         if button.flag:
             self.get_umc_url()
@@ -80,6 +113,12 @@ class umc(Page):
             return True
 
     def click_deactivate(self) -> bool:
+        """
+        This method clicks the deactivate button.
+
+        Returns:
+            bool: False if the button is clicked, True otherwise.
+        """
         button = self.search_by_xpath(self.deactivate_button)
         if button.flag:
             button.click()
@@ -89,50 +128,101 @@ class umc(Page):
             return True
 
     def click_edit(self) -> None:
+        """
+        This method clicks the edit button.
+        """
         self.search_by_xpath(self.edit_button).click()
 
     def click_remove_role(self) -> None:
+        """
+        This method clicks the remove role button.
+        """
         remove_role = self.role_palette + self.remove_role_button
         remove_button = self.search_by_xpath(remove_role)
         return remove_button.click()
 
     def click_add_role(self) -> None:
+        """
+        This method clicks the add role button.
+        """
         add_role = self.role_palette + self.add_role_button
         add_button = self.search_by_xpath(add_role)
         return add_button.click()
 
     def click_save(self) -> bool:
+        """
+        This method clicks the save button.
+
+        Returns:
+            bool: True if the button is clicked, False otherwise.
+        """
         return self.search_by_xpath(self.save_button).click()
 
     def search_activate_button(self) -> bool:
+        """
+        This method searches for the activate button.
+
+        Returns:
+            bool: False if the button is flagged, True otherwise.
+        """
         deactivate = self.search_by_xpath(self.deactivate_button)
         return not deactivate.flag
 
     def get_details_account_status(self) -> str:
+        """
+        This method gets the details of the account status.
+
+        Returns:
+            str: The text of the status element.
+        """
         status = self.search_by_xpath(self.account_status_field, delay=0.5)
         if status.flag:
             element = status.return_element()
             return element.text
-        
+
     def get_search_account_status(self) -> str:
+        """
+        This method gets the search account status.
+
+        Returns:
+            str: The text of the status element.
+        """
         status = self.search_by_xpath(self.search_result_status, delay=0.5)
         if status.flag:
             element = status.return_element()
             return element.text
 
     def search_first_owned_role(self) -> bool:
+        """
+        This method searches for the first owned role.
+
+        Returns:
+            bool: True if the role is found, False otherwise.
+        """
         role_selected_field = self.search_by_xpath(self.first_owned_role)
         return role_selected_field.flag
 
     def select_first_role(self) -> bool:
+        """
+        This method selects the first role.
+
+        Returns:
+            bool: True if the role is selected, False otherwise.
+        """
         role_selected_field = self.search_by_xpath(self.first_owned_role)
         if role_selected_field.flag:
             element = role_selected_field.click()
             return True
         else:
             return False
-        
+
     def verify_updated_role(self) -> bool:
+        """
+        This method verifies if the role is updated.
+
+        Returns:
+            bool: True if the role is updated, False otherwise.
+        """
         updated_notif = self.search_by_xpath(self.feedback_panel)
 
         if updated_notif.flag:
@@ -141,12 +231,30 @@ class umc(Page):
         return False
 
     def select_role(self, role: str) -> bool:
+        """
+        This method selects a role.
+
+        Args:
+            role (str): The role to select.
+
+        Returns:
+            bool: True if the role is selected, False otherwise.
+        """
         suffix = self.role_palette_suffix.replace("replaced_text",role)
         xpath = self.role_palette + suffix
 
         return self.search_by_xpath(xpath=xpath).click()
 
     def select_owned_role(self, role: str) -> bool:
+        """
+        This method selects an owned role.
+
+        Args:
+            role (str): The role to select.
+
+        Returns:
+            bool: True if the role is selected, False otherwise.
+        """
         role_suffix = self.role_palette_suffix.replace("replaced_text",role)
         xpath = self.owned_role_prefixed + role_suffix
         owned_role = self.search_by_xpath(xpath=xpath)
--- pages/1_UMC Automation Scripts.py
+++ pages/1_UMC Automation Scripts.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
-from Activity.umc_actions import *
+from Activity.umc_actions import (
+    login_to_site,
+    add_homesis_homesis_user,
+    deactivate_user_with_reason,
+    reactivate_user,
+)
 
 
 def main():
@@ -34,36 +39,36 @@ def main():
 
     # List HR Code/Login Name Input
     csv_upload = st.file_uploader(
-        label="HR Code/Log In Name List", type=["csv", "txt"], accept_multiple_files=False
+        label="HR Code/Log In Name List",
+        type=["csv", "txt"],
+        accept_multiple_files=False,
     )
 
     # Create two columns for Active & Reactive
-    
-    active_col, fil_col, reactive_col = st.columns(3)
-  
+
+    active_col, _, reactive_col = st.columns(3)
+
     # Activate Button on the left Column
     active_account_button = active_col.button("Activate Accounts")
     # Reactivate Button on the right Column
     reactivate_account_button = reactive_col.button("Reactivate Accounts")
 
     # Create two columns for Deactivation Reason & Deactivate Button
-    reason_col, button_col = st.columns([4,2])
-    
+    reason_col, button_col = st.columns([4, 2])
+
     # Reason Selection in the left column:
     deact_reason = reason_col.radio(
         label="What is the reason for Deactivation",
         options=options,
         index=0,
         captions=captions,
-        key = "reason_selection"
+        key="reason_selection",
     )
 
     # Deactivate Button in the right column
     remove_roles_button = button_col.button(
-        "Deactivate Accounts",
-        key = "deactivate_button")
-
-  
+        "Deactivate Accounts", key="deactivate_button"
+    )
 
     # Read CSV Data
     if csv_upload is not None:
@@ -81,7 +86,6 @@ def main():
             add_homesis_homesis_user(umc_page=umc_page, hr_code=hr_code)
             umc_page.get_umc_url()
 
-
     # Deactivate Account
     if remove_roles_button:
         # Start Selenium
@@ -91,7 +95,9 @@ def main():
         for index, row in csv_data.iterrows():
             hr_code = row["HR Code"]
             reason = options[options.index(deact_reason)]
-            deactivate_user_with_reason(umc_page=umc_page, hr_code=hr_code, reason=reason)
+            deactivate_user_with_reason(
+                umc_page=umc_page, hr_code=hr_code, reason=reason
+            )
             umc_page.get_umc_url()
 
     # Reactivate Account

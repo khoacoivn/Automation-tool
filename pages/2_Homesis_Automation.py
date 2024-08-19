0add34080b5aa1ced95aeb1f172ef161b526f876
NhuHuynh <nhu.huynhny@homecredit.vn>
2024-08-19 13:30:41 +0700

    update fully code with simple log function

--- pages/2_Homesis_Automation.py
+++ pages/2_Homesis_Automation.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 from Activity.homesis_actions import (
     login_to_site,
-    add_role_in_bank_RA_function,
-    add_role_in_bank_RA_MW_function,
-    add_role_in_bank_SA_function,
-    add_role_in_bank_RA_FPT_function,
-    add_role_in_bank_RA_New_Segment_function
+    add_role_in_bank_RA,
+    add_role_in_bank_RA_MW,
+    add_role_in_bank_SA,
+    add_role_in_bank_RA_FPT,
+    add_role_in_bank_RA_New_Segment
 )
 
 def main():
@@ -30,7 +30,7 @@ def main():
     
     # Read CSV Data
     if csv_upload_homesis_template is not None:
-        csv_data = pd.read_csv(csv_upload_homesis_template, converters={"HR Code": str, "ID number" : str, "Supervisor code" : str})
+        csv_data = pd.read_csv(csv_upload_homesis_template, converters={"HR Code": str, "ID number" : str, "Supervisor code" : str,  "Notes" : str, "Role in Bank" : str, "Location" : str})
         result_table = st.write(csv_data)
 
 
@@ -45,9 +45,9 @@ def main():
         st.markdown("Required field:")
         st.text("HR Code, ID number, Notes, Sup Code, Role SA, Location")
         #Add role in bank SA button
-        add_role_in_bank_SA = st.button("Add role-in-bank SA")
+        add_role_in_bank_SA_btn = st.button("Add role-in-bank SA")
         #Add role in bank SA for Homesis page
-        if add_role_in_bank_SA:
+        if add_role_in_bank_SA_btn:
             # Start Selenium
             homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
             homesis_page.access_user_managerment()
@@ -60,17 +60,19 @@ def main():
                 supervisor = row["Supervisor code"]
                 role = row["Role in Bank"]
                 location = row["Location"]
-                add_role_in_bank_SA_function(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, supervisor_code= supervisor, role = role, location = location)
+                list_error = add_role_in_bank_SA(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, supervisor_code= supervisor, role = role, location = location)
+                st.write(list_error)
                 homesis_page.get_homesis_url()
-                homesis_page.access_user_managerment()        
+                homesis_page.access_user_managerment()
+
 
     with tab2:
         st.markdown("Required field:")
         st.text("HR Code, ID number, Notes, Sup Code, Role RA")
         #Add role in bank RA button
-        add_role_in_bank_RA = st.button("Add role-in-bank RA")
+        add_role_in_bank_RA_btn = st.button("Add role-in-bank RA")
         #Add role in bank RA for Homesis page
-        if add_role_in_bank_RA:
+        if add_role_in_bank_RA_btn:
             # Start Selenium
             homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
             homesis_page.access_user_managerment()
@@ -82,16 +84,19 @@ def main():
                 note = row["Notes"]
                 supervisor = row["Supervisor code"]
                 role = row["Role in Bank"]
-                add_role_in_bank_RA_function (homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, supervisor_code= supervisor, role = role)
+                list_error = add_role_in_bank_RA (homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, supervisor_code= supervisor, role = role)
+                st.write(list_error)
                 homesis_page.get_homesis_url()
-                homesis_page.access_user_managerment()        
+                homesis_page.access_user_managerment()
+
+     
     with tab3:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA")
-        #Add role in bank RA button
-        add_role_in_bank_RA_MW = st.button("Add role-in-bank RA MW")
+        #Add role in bank RA MW button
+        add_role_in_bank_RA_MW_btn = st.button("Add role-in-bank RA MW")
         #Add role in bank RA MW for Homesis page
-        if add_role_in_bank_RA_MW:
+        if add_role_in_bank_RA_MW_btn:
             # Start Selenium
             homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
             homesis_page.access_user_managerment()
@@ -101,16 +106,19 @@ def main():
                 hr_code = row["HR Code"]
                 note = row["Notes"]
                 role = row["Role in Bank"]
-                add_role_in_bank_RA_MW_function(homesis_page = homesis_page, hr_code = hr_code, note = note, role = role)
+                list_error = add_role_in_bank_RA_MW(homesis_page = homesis_page, hr_code = hr_code, note = note, role = role)
+                st.write(list_error)
                 homesis_page.get_homesis_url()
                 homesis_page.access_user_managerment()
+
+
     with tab4:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA, ID number")
-        #Add role in bank RA button
-        add_role_in_bank_RA_FPT = st.button("Add role-in-bank RA FPT")
+        #Add role in bank RA FPT button
+        add_role_in_bank_RA_FPT_btn = st.button("Add role-in-bank RA FPT")
         #Add role in bank RA FPT for Homesis page
-        if add_role_in_bank_RA_FPT:
+        if add_role_in_bank_RA_FPT_btn:
             # Start Selenium
             homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
             homesis_page.access_user_managerment()
@@ -121,16 +129,19 @@ def main():
                 id_number = row["ID number"]
                 note = row["Notes"]
                 role = row["Role in Bank"]
-                add_role_in_bank_RA_FPT_function(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, role = role)
+                list_error = add_role_in_bank_RA_FPT(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, role = role)
+                st.write(list_error)
                 homesis_page.get_homesis_url()
                 homesis_page.access_user_managerment()
+
+
     with tab5:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA, ID number, Sup Code")
         #Add role in bank RA button
-        add_role_in_bank_RA_NS = st.button("Add role-in-bank RA New Segment")
-        #Add role in bank RA FPT for Homesis page
-        if add_role_in_bank_RA_NS:
+        add_role_in_bank_RA_NS_btn = st.button("Add role-in-bank RA New Segment")
+        #Add role in bank RA New Segment for Homesis page
+        if add_role_in_bank_RA_NS_btn:
             # Start Selenium
             homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
             homesis_page.access_user_managerment()
@@ -141,9 +152,13 @@ def main():
                 id_number = row["ID number"]
                 note = row["Notes"]
                 role = row["Role in Bank"]
-                add_role_in_bank_RA_New_Segment_function(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, role = role)
+                supervisor = row["Supervisor code"]
+                list_error = add_role_in_bank_RA_New_Segment(homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, role = role, supervisor_code= supervisor)
+                st.write(list_error)
                 homesis_page.get_homesis_url()
-                homesis_page.access_user_managerment()                
+                homesis_page.access_user_managerment()
+           
+            
  
 
 if __name__ == "__main__":

8bf7ecb9965119bf78e4d21298b2894fd2a96df9
NhuHuynh <nhu.huynhny@homecredit.vn>
2024-08-23 09:53:32 +0700

    add new feature

--- pages/2_Homesis_Automation.py
+++ pages/2_Homesis_Automation.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
+from Common.constant.css_file import CSS
 
 from Activity.homesis_actions import (
     login_to_site,
-    add_role_in_bank_RA,
     add_role_in_bank_RA_MW,
     add_role_in_bank_SA,
     add_role_in_bank_RA_FPT,
-    add_role_in_bank_RA_New_Segment
+    add_role_in_bank_RA_New_Segment,
+    change_role_in_bank
 )
 
 def main():
@@ -40,12 +41,12 @@ def main():
     #Chose action on Homesis page
     section_divided_caption = st.subheader("Add role in bank", divider= "red")
 
-    tab1, tab2, tab3, tab4, tab5 = st.tabs(["SA Home", "RA Basic", "RA MW", "RA FPT", "RA NS"])
+    tab1, tab2, tab3, tab4 = st.tabs(["SA Home", "RA MW", "RA FPT", "RA NS"])
     with tab1:
         st.markdown("Required field:")
         st.text("HR Code, ID number, Notes, Sup Code, Role SA, Location")
         #Add role in bank SA button
-        add_role_in_bank_SA_btn = st.button("Add role-in-bank SA")
+        add_role_in_bank_SA_btn = st.button("Add role-in-bank SA", type="primary")
         #Add role in bank SA for Homesis page
         if add_role_in_bank_SA_btn:
             # Start Selenium
@@ -65,36 +66,12 @@ def main():
                 homesis_page.get_homesis_url()
                 homesis_page.access_user_managerment()
 
-
+   
     with tab2:
-        st.markdown("Required field:")
-        st.text("HR Code, ID number, Notes, Sup Code, Role RA")
-        #Add role in bank RA button
-        add_role_in_bank_RA_btn = st.button("Add role-in-bank RA")
-        #Add role in bank RA for Homesis page
-        if add_role_in_bank_RA_btn:
-            # Start Selenium
-            homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
-            homesis_page.access_user_managerment()
-        
-            # Loop through CSV & Search for HR Code and take data from CSV
-            for index, row in csv_data.iterrows():
-                hr_code = row["HR Code"]
-                id_number = row["ID number"]
-                note = row["Notes"]
-                supervisor = row["Supervisor code"]
-                role = row["Role in Bank"]
-                list_error = add_role_in_bank_RA (homesis_page = homesis_page, hr_code = hr_code, id_number = id_number, note = note, supervisor_code= supervisor, role = role)
-                st.write(list_error)
-                homesis_page.get_homesis_url()
-                homesis_page.access_user_managerment()
-
-     
-    with tab3:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA")
         #Add role in bank RA MW button
-        add_role_in_bank_RA_MW_btn = st.button("Add role-in-bank RA MW")
+        add_role_in_bank_RA_MW_btn = st.button("Add role-in-bank RA MW", type="primary")
         #Add role in bank RA MW for Homesis page
         if add_role_in_bank_RA_MW_btn:
             # Start Selenium
@@ -112,11 +89,11 @@ def main():
                 homesis_page.access_user_managerment()
 
 
-    with tab4:
+    with tab3:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA, ID number")
         #Add role in bank RA FPT button
-        add_role_in_bank_RA_FPT_btn = st.button("Add role-in-bank RA FPT")
+        add_role_in_bank_RA_FPT_btn = st.button("Add role-in-bank RA FPT", type="primary")
         #Add role in bank RA FPT for Homesis page
         if add_role_in_bank_RA_FPT_btn:
             # Start Selenium
@@ -135,11 +112,11 @@ def main():
                 homesis_page.access_user_managerment()
 
 
-    with tab5:
+    with tab4:
         st.markdown("Required field:")
         st.text("HR Code, Notes, Role RA, ID number, Sup Code")
         #Add role in bank RA button
-        add_role_in_bank_RA_NS_btn = st.button("Add role-in-bank RA New Segment")
+        add_role_in_bank_RA_NS_btn = st.button("Add role-in-bank RA New Segment", type="primary")
         #Add role in bank RA New Segment for Homesis page
         if add_role_in_bank_RA_NS_btn:
             # Start Selenium
@@ -158,8 +135,53 @@ def main():
                 homesis_page.get_homesis_url()
                 homesis_page.access_user_managerment()
            
-            
- 
+    section_divided_caption_other_action = st.subheader("Update Homesis information", divider= "red")
+
+    Change_role_in_bank, Add_sup_code, other = st.tabs(["Change Role-in-bank","Add sup code", "Other"])
+
+    with Change_role_in_bank:
+        left, rigth = st.columns(2, vertical_alignment="bottom")
+
+        #Upload list Hr code 
+        csv_upload_hrcode_change_role_in_bank= left.file_uploader(
+        label="Please input list of HR code you want to change role in bank",
+        type=["csv", "txt"],
+        accept_multiple_files=False,
+        
+        )
+        # Read CSV Data
+        if csv_upload_hrcode_change_role_in_bank is not None:
+            csv_data_change_role_in_bank = pd.read_csv(csv_upload_hrcode_change_role_in_bank, converters={"HR Code": str})
+               
+        # Select the option for change the role in bank
+        option = rigth.selectbox(
+        "Chose a role-in-bank",
+        (   "** choose **",
+            "SA", 
+            "RA",
+            "Telesales operator"),
+        index=None,
+        )
+        
+        #Input css for upload file in change role in bank section
+        st.markdown(CSS.homesis_css.css, unsafe_allow_html=True)
+
+        #Change role in bank button
+        change_role_in_bank_btn = st.button("Change Role-in-Bank", type= "primary")
+        #Change role in bank action
+        if change_role_in_bank_btn:
+            # Start Selenium
+            homesis_page = login_to_site(ldap_user=ldap_user, ldap_pw=ldap_pw)
+            homesis_page.access_user_managerment()
+        
+            # Loop through CSV & Search for HR Code and take data from CSV
+            for index, row in csv_data_change_role_in_bank.iterrows():
+                hr_code = row["HR Code"]
+                role = option
+                change_role_in_bank(homesis_page = homesis_page, hr_code = hr_code, role = option)
+                homesis_page.get_homesis_url()
+                homesis_page.access_user_managerment() 
+
 
 if __name__ == "__main__":
     main()

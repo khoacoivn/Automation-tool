0a4faf5215ac5f43f5c4dd436cb8234684ebcda1
Nhu.HuynhNY <nhu.huynhny@homecredit.vn>
2024-08-26 08:38:33 +0200

    Nhu huynh.feature update note and add sup code

---
+++ Common/constant/css_file.py
@@ -0,0 +1,22 @@
+#homesis
+class homesis_css:
+        #CSS to style a upload file button in change role in bank section
+        css = '''
+        <style>
+            div[data-testid="column"]:has(>div>div>div>div[data-testid="element-container"] >div[data-testid='stFileUploader']) {
+                width: max-content;
+            }
+            div[data-testid="column"]:has(>div>div>div>div[data-testid="element-container"] >div[data-testid='stFileUploader']) section {
+                padding: 0;
+                float: left;
+            }
+            div[data-testid="column"]:has(>div>div>div>div[data-testid="element-container"] >div[data-testid='stFileUploader']) section > input + div {
+                display: none;
+            }
+            div[data-testid="column"]:has(>div>div>div>div[data-testid="element-container"] >div[data-testid='stFileUploader']) section + div {
+                float: right;
+                padding-top: 0;
+            }
+
+        </style>
+         '''
